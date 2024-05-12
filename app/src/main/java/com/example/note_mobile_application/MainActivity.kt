40142a86f47d637ca869b8aa0c990163119e3d11
NDilusha <161825980+IT22171092RANDRanasingha@users.noreply.github.com>
2024-05-12 19:19:19 +0530

    retrive th dat

--- app/src/main/java/com/example/note_mobile_application/MainActivity.kt
+++ app/src/main/java/com/example/note_mobile_application/MainActivity.kt
@@ -6,16 +6,25 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.note_mobile_application.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var db:NoteDatabaseHelper
+    private lateinit var `notesAdapter`: NotesAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
         setContentView(binding.root)
+
+        db = NoteDatabaseHelper(this)
+        notesAdapter = NotesAdapter(db.getAllNotes(),this)
+
+        binding.notesRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.notesRecyclerView.adapter = notesAdapter
 
         binding.addButton.setOnClickListener{
             val intent = Intent(this, AddNoteActivity::class.java)
@@ -26,5 +35,9 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+    override fun onResume() {
+        super.onResume()
+        notesAdapter.refreshData(db.getAllNotes())
     }
 }
